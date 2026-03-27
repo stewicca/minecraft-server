3696fce643d9e1e35ffddeef373d055d963fb3b5
stewicca <stewicalf@gmail.com>
2026-03-27 08:36:05 +0000

    Update from Spigot to Paper

--- setup.sh
+++ setup.sh
@@ -5,13 +5,13 @@ chmod +x ./backup.sh
 cp .env.example .env
 
 . ./.env
-sed -i "s|  server-name: .*|  server-name: \"${SERVER_NAME}\"|" ./data/plugins/Geyser-Spigot/config.yml
+sed -i "s|  server-name: .*|  server-name: \"${SERVER_NAME}\"|" ./data/plugins/Geyser-Paper/config.yml
 
-curl -L -o ./data/plugins/Geyser-Spigot.jar https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot --keepalive-time 60 &
+curl -L -o ./data/plugins/Geyser-Paper.jar https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot --keepalive-time 60 &
 curl -L -o ./data/plugins/floodgate-spigot.jar https://download.geysermc.org/v2/projects/floodgate/versions/latest/builds/latest/downloads/spigot --keepalive-time 60 &
 curl -L -o ./data/plugins/ViaVersion.jar https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/5.7.2/PAPER/ViaVersion-5.7.2.jar --keepalive-time 60 &
 wait
 
-ln -sf ../floodgate/key.pem ./data/plugins/Geyser-Spigot/key.pem
+ln -sf ../floodgate/key.pem ./data/plugins/Geyser-Paper/key.pem
 
 docker compose up -d
--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 docker compose down
 
-curl -L -o ./data/plugins/Geyser-Spigot.jar https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot --keepalive-time 60 &
+curl -L -o ./data/plugins/Geyser-Paper.jar https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot --keepalive-time 60 &
 curl -L -o ./data/plugins/floodgate-spigot.jar https://download.geysermc.org/v2/projects/floodgate/versions/latest/builds/latest/downloads/spigot --keepalive-time 60 &
 wait
 
