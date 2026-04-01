2e82cdc8807bcbfbcf597dca461f9215988baf67
stewicca <stewicalf@gmail.com>
2026-04-01 00:47:02 +0000

    Add EssentialsX and Vault plugin downloads to setup and update scripts

--- setup.sh
+++ setup.sh
@@ -9,11 +9,15 @@ sed -i "s|  server-name: .*|  server-name: \"${SERVER_NAME}\"|" ./data/plugins/G
 
 VIAVERSION=$(curl -s https://hangar.papermc.io/api/v1/projects/ViaVersion/latestrelease)
 LUCKPERMS=$(curl -s https://metadata.luckperms.net/data/all | grep -o '"bukkit":"[^"]*"' | cut -d'"' -f4)
+ESSENTIALSX=$(curl -s https://api.github.com/repos/EssentialsX/Essentials/releases/latest | grep -o '"browser_download_url": "https://github\.com[^"]*EssentialsX-[0-9][0-9.]*\.jar"' | grep -o 'https://[^"]*')
+VAULT=$(curl -s https://api.github.com/repos/MilkBowl/Vault/releases/latest | grep -o '"browser_download_url": "https://github\.com[^"]*\.jar"' | head -1 | grep -o 'https://[^"]*')
 
 curl -L -o ./data/plugins/Geyser-Paper.jar https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot --keepalive-time 60 &
 curl -L -o ./data/plugins/floodgate-spigot.jar https://download.geysermc.org/v2/projects/floodgate/versions/latest/builds/latest/downloads/spigot --keepalive-time 60 &
 curl -L -o ./data/plugins/ViaVersion.jar "https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/${VIAVERSION}/PAPER/ViaVersion-${VIAVERSION}.jar" --keepalive-time 60 &
 curl -L -o ./data/plugins/LuckPerms.jar "${LUCKPERMS}" --keepalive-time 60 &
+curl -L -o ./data/plugins/EssentialsX.jar "${ESSENTIALSX}" --keepalive-time 60 &
+curl -L -o ./data/plugins/Vault.jar "${VAULT}" --keepalive-time 60 &
 wait
 
 ln -sf ../floodgate/key.pem ./data/plugins/Geyser-Paper/key.pem
